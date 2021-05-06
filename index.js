9a25f6af56494b5b1db3fafc90722fdc53603c5a
ChaseTheCoder <chasefsheaff@gmail.com>
2021-05-05 22:59:37 -0500

    attempting to connect MongoDB, 'ExperimentalWartning' when npm start

--- index.js
+++ index.js
@@ -3,4 +3,21 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
- 
+const app = express();  // it's an object, oop, copy of express
+
+app.use(express.json({limit: "20mb", extended: true})); //limit of 20mp picture, extended true means it will only accept strings
+app.use(express.urlencoded({limit: "20mb", extended: true}));
+
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://mrSheaff:Pizza2000aws@teacherspetcluster.78om8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+  useNewUrlParser:true, useUnifiedTopology:true 
+}).then(() => app.listen(port, () =>
+console.log(`Connection is established and running on port: ${PORT}`)
+)).catch((err) => console.log(err.message));
+
+mongoose.set('useFindAndModify',false);
