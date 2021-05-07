1a36328aa7a269e821bd50a2c672fc974fa46387
ChaseTheCoder <chasefsheaff@gmail.com>
2021-05-06 20:22:52 -0500

    modifed index.js to add routes and created routes/students.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import studentRoutes from './routes/student.js';
 
 const app = express();  // it's an object, oop, copy of express
 
+app.use('/students', studentRoutes);
 app.use(express.json({limit: "20mb", extended: true})); //limit of 20mp picture, extended true means it will only accept strings
 app.use(express.urlencoded({limit: "20mb", extended: true}));
 
@@ -15,8 +17,8 @@ const CONNECTION_URL = 'mongodb+srv://mrSheaff:Pizza2000aws@teacherspetcluster.7
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {
-  useNewUrlParser:true, useUnifiedTopology:true 
-}).then(() => app.listen(port, () =>
+  useNewUrlParser:true, useUnifiedTopology:true  
+}).then(() => app.listen(PORT, () =>
 console.log(`Connection is established and running on port: ${PORT}`)
 )).catch((err) => console.log(err.message));
 
